2a871dfd97889b0c9cc59e7861c03ea903083631
Atul Sharrma <atulsharma@accuraticssoftwares.com>
2025-02-12 21:32:32 -0600

    SignOut Implemented

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,8 +4,14 @@ import { checkValidData } from "../utils/validate";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { updateProfile } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [existingUser, setExistingUser] = useState(true);
   const name = useRef(null);
   const email = useRef(null);
@@ -35,7 +41,30 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
+          updateProfile(auth.currentUser, {
+            displayName: name.current.value,
+            photoURL:
+              "https://media.licdn.com/dms/image/v2/C4E03AQGK3F8MvQFEQQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1621360162249?e=1744848000&v=beta&t=0SsCUYh7dVf-tdacSOmGRXMFnmozIeeE8lTq7Rx3MpU",
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = userCredential.user;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              // ...
+              setErrmsg(error.message);
+            });
           console.log(user);
+          navigate("/browse");
           // ...
         })
         .catch((error) => {
@@ -55,6 +84,7 @@ const Login = () => {
           // Signed in
           const user = userCredential.user;
           console.log(user);
+          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -63,6 +93,62 @@ const Login = () => {
         });
     }
   };
+
+  // const handleButtonClick = async () => {
+  //   const err = checkValidData(email.current.value, password.current.value);
+  //   setErrmsg(err);
+  //   if (err) return;
+
+  //   if (!existingUser) {
+  //     try {
+  //       // ✅ Sign-Up User
+  //       const userCredential = await createUserWithEmailAndPassword(
+  //         auth,
+  //         email.current.value,
+  //         password.current.value
+  //       );
+
+  //       const user = userCredential.user;
+
+  //       // ✅ Wait for profile update
+  //       await updateProfile(user, {
+  //         displayName: name.current.value,
+  //         photoURL:
+  //           "https://media.licdn.com/dms/image/v2/C4E03AQGK3F8MvQFEQQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1621360162249?e=1744848000&v=beta&t=0SsCUYh7dVf-tdacSOmGRXMFnmozIeeE8lTq7Rx3MpU",
+  //       });
+
+  //       console.log("User Profile Updated:", user);
+
+  //       dispatch(
+  //         addUser({
+  //           uid: user.uid,
+  //           email: user.email,
+  //           displayName: name.current.value,
+  //           photoURL: user.photoURL, // Updated photoURL
+  //         })
+  //       );
+
+  //       // ✅ Now navigate after profile is updated
+  //       navigate("/browse");
+  //     } catch (error) {
+  //       setErrmsg(error.code + " - " + error.message);
+  //     }
+  //   } else {
+  //     try {
+  //       // ✅ Sign-In User
+  //       const userCredential = await signInWithEmailAndPassword(
+  //         auth,
+  //         email.current.value,
+  //         password.current.value
+  //       );
+
+  //       console.log("User Signed In:", userCredential.user);
+  //       navigate("/browse");
+  //     } catch (error) {
+  //       setErrmsg(error.code + " - " + error.message);
+  //     }
+  //   }
+  // };
 
   return (
     <>
